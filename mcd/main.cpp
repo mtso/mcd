b6b4f2ad9f1855313da95e0aa05564ca239edfb0
mtso <mtso@tuta.io>
2016-11-15 23:26:58 -0800

    Initial Commit

---
+++ mcd/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mcd
+//
+//  Created by Matthew Tso on 11/15/16.
+//  Copyright © 2016 Matthew Tso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
